1fef9643c09ff53d7351384e589f33ccc4e22b3c
Antonio8315 <berehii.anton@chnu.edu.ua>
2024-08-20 10:29:26 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+import java.lang.Math;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Lab 1 Java");
-        System.out.printf("test");
+        System.out.println("Task 1:");
+        System.out.print("Введіть число x: "); //ціле
+        int m = scanner.nextInt();
+
+        // Запитуємо друге число
+        System.out.print("Введіть число y: "); //ціле
+        int n = scanner.nextInt();
+
+        double dod1 = (n+1) / Math.pow(n, 2) * n +2;
+        double dod2 = (m+n) / (m-n);
+        double answer = dod1 + dod2;
+        System.out.print("Answer: " + answer);
     }
 }
